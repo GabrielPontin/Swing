d9c54cd48a57db73fdc4bbd147c3788f418f63f8
GabrielPontin <gabrielpontin01@gmail.com>
2016-05-31 19:33:32 -0300

    criando tela principal da tabela

---
+++ src/br/univel/TelaPrincipal.java
@@ -0,0 +1,21 @@
+package br.univel;
+
+public class TelaPrincipal {
+
+	public class TelaPrincipal extends Jfram{
+		
+		public static void main(String[]args){
+			EventQueue.invokeLater(new Runnable(){
+				
+			}
+		}
+
+	}
+
+
+	public TelaPrincipal(){
+		JPanel jp = new JPanel();
+		jp.setLayout(new BorderLayout());
+		
+	}
+}
